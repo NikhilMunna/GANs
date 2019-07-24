f2b349fa01d2b778b18017d6023ac3bd485b1aa9
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 12:18:10 +0530

    added utils

---
+++ utils.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import errno
+import torchvision.utils as vutils
+from tensorboardX import SummaryWriter
+from IPython import display
+from matplotlib import pyplot as plt
+import torch
+
+def log(self, d_error, g_error, epoch, n_batch, num_batches):
+
+        # var_class = torch.autograd.variable.Variable
+        if isinstance(d_error, torch.autograd.Variable):
+            d_error = d_error.data.cpu().numpy()
+        if isinstance(g_error, torch.autograd.Variable):
+            g_error = g_error.data.cpu().numpy()
+
+        step = Logger._step(epoch, n_batch, num_batches)
+        self.writer.add_scalar(
+            '{}/D_error'.format(self.comment), d_error, step)
+        self.writer.add_scalar(
+            '{}/G_error'.format(self.comment), g_error, step)
