080d50770fb944c65109a7156af609d1219ad646
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 12:34:35 +0530

    added discriminator trainer

--- gan.py
+++ gan.py
@@ -118,3 +118,27 @@ def zeros_target(size):
     '''
     data = Variable(torch.zeros(size, 1))
     return data
+
+
+def train_discriminator(optimizer, real_data, fake_data):
+    N = real_data.size(0)
+    # Reset gradients
+    optimizer.zero_grad()
+    
+    # 1.1 Train on Real Data
+    prediction_real = discriminator(real_data)
+    # Calculate error and backpropagate
+    error_real = loss(prediction_real, ones_target(N) )
+    error_real.backward()
+
+    # 1.2 Train on Fake Data
+    prediction_fake = discriminator(fake_data)
+    # Calculate error and backpropagate
+    error_fake = loss(prediction_fake, zeros_target(N))
+    error_fake.backward()
+    
+    # 1.3 Update weights with gradients
+    optimizer.step()
+    
+    # Return error and predictions for real and fake inputs
+    return error_real + error_fake, prediction_real, prediction_fake
