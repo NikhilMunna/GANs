ccf0a046a9b3119bec75306b7ec0ae41567219fa
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 12:29:31 +0530

    created generative  model

--- gan.py
+++ gan.py
@@ -50,3 +50,39 @@ class DiscriminatorNet(torch.nn.Module):
         x = self.hidden2(x)
         x = self.out(x)
         return xdiscriminator = DiscriminatorNet()
+
+
+
+class GeneratorNet(torch.nn.Module):
+    """
+    A three hidden-layer generative neural network
+    """
+    def __init__(self):
+        super(GeneratorNet, self).__init__()
+        n_features = 100
+        n_out = 784
+        
+        self.hidden0 = nn.Sequential(
+            nn.Linear(n_features, 256),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden1 = nn.Sequential(            
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2)
+        )
+        self.hidden2 = nn.Sequential(
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2)
+        )
+        
+        self.out = nn.Sequential(
+            nn.Linear(1024, n_out),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.hidden0(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.out(x)
+        return xgenerator = GeneratorNet()
